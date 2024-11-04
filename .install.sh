931c02e843b1daecab163523cf14681eebc51446
Deepak Agarwal <deepakagarwal@Deepaks-MacBook-Pro.local>
2024-11-04 12:37:27 +0530

    inital-commit

---
+++ .install.sh
@@ -0,0 +1,76 @@
+#!/bin/zsh
+
+# Install xCode cli tools
+echo "Installing commandline tools..."
+xcode-select --install
+
+# Homebrew
+## Install
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew analytics off
+
+## Taps
+echo "Tapping Brew..."
+brew tap homebrew/cask-fonts
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
+## Formulae
+echo "Installing Brew Formulae..."
+### Essentials
+brew install skhd
+brew install sketchybar
+brew install borders
+brew install yabai
+
+### Terminal
+brew install starship
+brew install zsh-autosuggestions
+brew install zsh-fast-syntax-highlighting
+
+## Casks
+echo "Installing Brew Casks..."
+### Terminals & Browsers
+brew install --cask alacritty
+brew install --cask arc
+
+## Development tools
+echo "Development Tools..."
+brew install --cask cursor
+brew install nvm
+brew install pyenv
+
+### Fonts
+brew install --cask sf-symbols
+brew install --cask font-hack-nerd-font
+brew install --cask font-jetbrains-mono
+brew install --cask font-fira-code
+
+# Copying and checking out configuration files
+echo "Planting Configuration Files..."
+[ ! -d "$HOME/.config" ] && git clone --bare git@github.com:deepak-agarwal/my-setup-mac.git $HOME/.config
+
+# Installing Fonts
+git clone git@github.com:shaunsingh/SFMono-Nerd-Font-Ligaturized.git /tmp/SFMono_Nerd_Font
+mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
+rm -rf /tmp/SFMono_Nerd_Font/
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+
+source $HOME/.zshrc
+cfg config --local status.showUntrackedFiles no
+
+
+# Start Services
+echo "Starting Services (grant permissions)..."
+brew services start skhd
+brew services start fyabai
+brew services start sketchybar
+brew services start borders
+
+csrutil status
+echo "(optional) Disable SIP for advanced yabai features."
+echo "(optional) Add sudoer manually:\n '$(whoami) ALL = (root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | awk "{print \$1;}") $(which yabai) --load-sa' to '/private/etc/sudoers.d/yabai'"
+echo "Installation complete...\n"
